804d37cffc4d7c4c5a457a3a3aa3fab591e13967
Abishek <abishekhari09@gmail.com>
2022-03-19 17:25:49 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector(".modal");
+const loginBtn = document.querySelector(".btn");
+const closeBtn = document.querySelector(".close");
+
+loginBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+
+function openModal() {
+  modal.style.display = "block";
+}
+function closeModal() {
+  modal.style.display = "none";
+}
